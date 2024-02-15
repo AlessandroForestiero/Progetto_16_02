aceade9791aa83c3125b2f9b0f2a33a231d628ca
AlessandroForestiero <forestieroalessandro94@gmail.com>
2024-02-15 18:09:16 +0100

    Initial commit

---
+++ src/main/java/com/example/progetto_16_02/Progetto1602Application.java
@@ -0,0 +1,13 @@
+package com.example.progetto_16_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto1602Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progetto1602Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/progetto_16_02/Progetto1602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progetto_16_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Progetto1602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
